5ab8cd42f9ad175a534cc369b6c2598626bb6a3d
StephaneCash <kikonistephane@gmail.com>
2022-03-08 14:46:14 +0100

    Form4 pass

--- src/pages/Form4.js
+++ src/pages/Form4.js
@@ -14,8 +14,11 @@ function Form4() {
     const [click, setClick] = useState(false);
     const [isvalidPwd, setIsValidPwd] = useState(false);
 
-    const [etatPwd, setEtatPwd] = useState(false);
+    const [etatBtnShowPass, setEtatBtnShowPass] = useState(false);
 
+    const [msgPwdVald, setMsgPwdVald] = useState("Veuillez créer un mot de passe svp !");
+
+    // Vérification si un élément chosi ou pas, la valeur su type de compte
     function handleTypeCompte(e) {
         if (e.target.value === "--Type de compte--") {
             setEtatSelect(false);
@@ -24,28 +27,42 @@ function Form4() {
         };;
     }
 
+    // Vérification de la force du moy de passe et voir si le input est vide ou pas
     function handlePwd(e) {
-        setEtatPwd(true);
         if (e.target.value === "") {
             setIsValidPwd(false);
-        } else {
+            setEtatBtnShowPass(true);
+            setMsgPwdVald('Veuillez créer un mot de passe svp !');
+        }
+        else if (e.target.value.match(/[0-9]/) && e.target.value.match(/[A-Z]/) && e.target.value.match(/[a-z]/)
+            && e.target.value.match(/[@,;:.*#/]/) && e.target.value.length > 8) {
             setIsValidPwd(true);
+            setMsgPwdVald('Mot de passe valide');
+        }
+        else {
+            setIsValidPwd(false);
+            setEtatBtnShowPass(isvalidPwd);
+            setMsgPwdVald("Votre mot de passe doit avoir au moins un chiffre, une lettre minuscule, une lettre majiscule, un caractère spécial et doit avoir la taille de plus ou égal à 8 caractères")
         };
     };
 
-    let num = 0;
-
+    // Fonction pour visualiser le mot de passe entré 
     const handleShowPass = () => {
-        num++;
-        if (num === 1)
-            setEtatPwd(false);
-        else
-            setEtatPwd(true);
+        if (userData.password) {
+            setEtatBtnShowPass(true);
+        } else {
+            setEtatBtnShowPass(false);
+        }
+        console.log(etatBtnShowPass)
     };
 
-    console.log(
-        num
-    )
+    if (userData.password) {
+        if (userData.password === confirmPass) {
+            //  console.log('Pass valid et égal')
+        } else {
+            // console.log('Pass différent')
+        }
+    }
 
     function submitData() {
         setClick(true);
@@ -64,7 +81,7 @@ function Form4() {
             setIsValidPwd(true);
         }
     }, []);
-    console.log("Data user : ", userData);
+    //console.log("Data user : ", userData);
 
     return (
         <>
@@ -110,23 +127,38 @@ function Form4() {
 
                             <OutlinedInput
                                 placeholder="Créer un mot de passe"
+                                helperText={"Pass"}
                                 id="outlined-adornment-password"
-                                type={etatPwd ? 'password' : 'text'}
+                                type={etatBtnShowPass === false ? 'password' : 'text'}
                                 value={userData['password']}
                                 style={{ width: '100%' }}
                                 onChange={(e) => (setUserData({ ...userData, 'password': e.target.value }), handlePwd(e))}
+                                inputProps={{
+                                    'aria-label': 'weight',
+                                }}
                                 endAdornment={
                                     <InputAdornment position="end">
                                         <IconButton
                                             aria-label="toggle password visibility"
+                                            onClick={handleShowPass}
                                             edge="end"
                                         >
-                                            {etatPwd ? <VisibilityOff onClick={handleShowPass} /> : <Visibility />}
+                                            {etatBtnShowPass === false ? <VisibilityOff /> : <Visibility />}
                                         </IconButton>
                                     </InputAdornment>
                                 }
                             />
-                            <br /><br />
+                            <br />
+                            {
+                                click === true ?
+                                    <>
+                                        {isvalidPwd === false ?
+                                            <div className="sexeOdbligatoireJust">{msgPwdVald}<br /><br /></div> :
+                                            <span className="sexeOdbligatoire" style={{ color: "green" }}>
+                                                {msgPwdVald} <br /><br /></span>}
+                                    </>
+                                    : isvalidPwd
+                            }
 
                             <label style={{ marginBottom: '10px' }}>Répéter le mot de passe:</label> <br />
                             <TextField
