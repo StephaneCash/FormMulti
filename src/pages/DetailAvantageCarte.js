ced86d02e47ba00ffe16a11d7969e2abc9c777a2
StephaneCash <kikonistephane@gmail.com>
2022-04-05 13:15:31 +0100

    s

--- src/pages/DetailAvantageCarte.js
+++ src/pages/DetailAvantageCarte.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function DetailAvantageCarte() {
   return (
+      
     <div className="col-12">
-        
         <h2 className="text-center">Comparer les cartes et leurs <br /> fonctionnalités</h2>
     </div>
   )
