864f63b7640f23c44cf587eab8bb5282c5c013a7
StephaneCash <kikonistephane@gmail.com>
2022-04-06 11:51:13 +0100

    detailAvantages

--- src/pages/DetailAvantageCarte.js
+++ src/pages/DetailAvantageCarte.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import prenium from "../images/prenium.png";
+import standardCard from "../images/standardCard.png";
 
 function DetailAvantageCarte() {
   return (
     <div className="col-12">
-        <h2 className="text-center">Comparer les cartes et leurs <br /> fonctionnalités</h2>
+      <h2 className="text-center">Comparer les cartes et leurs <br /> fonctionnalités</h2>
+
+      <div className="col-12">
+        <div className="row">
+          <div className="col-6"></div>
+          <div className="col-6">
+            <div className="d-flex">
+              <div className="col-4">
+                <div className="col-12">
+                  <img src={standardCard} className="imageCardStandard" />
+                </div>
+                <div className="col-12">
+                  Carte Standard
+                </div>
+              </div>
+              <div className="col-4">
+                <img src={prenium} className="imageCardStandard" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
