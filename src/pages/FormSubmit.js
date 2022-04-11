1885a80d8154466d4e649b8ab7d97d55140ef0ef
StephaneCash <kikonistephane@gmail.com>
2022-04-11 16:15:24 +0100

    toast

--- src/pages/FormSubmit.js
+++ src/pages/FormSubmit.js
@@ -1,9 +1,13 @@
 import React, {useContext} from 'react';
 import { multiStepContext } from "../StepContext";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function FormSubmit() {
 
     const { userData } = useContext(multiStepContext);
+
+    toast.success("Vos informations ont été soumises avec succès");
 
     return (
         <div>
